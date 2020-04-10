96536c9c376a48b33819849b02b0776e8531e6d8
rupaodedara <rupa.odedara.fatah@gmail.com>
2020-04-10 17:28:05 +0530

    apply new hover style on image

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $( function() {
+
+
+
+
+
+
   // init plugin
   NodeCursor({
       cursor : true, 
@@ -23,7 +29,10 @@ $( function() {
   tl.from('span', 1, { width: 0}, "=-.5");
   tl.from('#office', 1, {x:-200, opacity: 0,ease: Power4.easeInOut}, "=-1");
   tl.from('#building', 1, {x:200, opacity: 0, ease: Power4.easeInOut}, "=-.7");
-  
+  tl.from('.circle-code', 1, { x:-100}, "=-.7");
+  tl.from('.circle-design', 1, { x:100 }, "=-.7");
+  tl.from('.circle-create', 1, { y:15 }, "=-.7");
+
   tl2.from("#box", 1, {opacity: 0, scale: 0});
   tl2.to("#box", .5, {left: "20%", scale: 1.3, borderColor: 'white', borderWidth: 12, boxShadow: '1px 1px 0px 0px rgba(0,0,0,0.09)'})
   
@@ -50,13 +59,40 @@ $( function() {
   }
   
   
-  const cursor = document.querySelector(".cursor");
+  const slider = document.querySelectorAll(".main");
+  const slide = document.querySelectorAll(".slide");
+  const slide1 = document.querySelectorAll(".slide1");
+  const slide2 = document.querySelectorAll(".slide2");
+  const slide3 = document.querySelectorAll(".slide3");
+  const slide4 = document.querySelectorAll(".slide4");
+  const slide5 = document.querySelectorAll(".slide5");
+  const slide6 = document.querySelectorAll(".slide6");
+  const headline =document.querySelectorAll(".headline");
+  const heroimg = document.querySelectorAll(".heroimg");
+  var tl3 = new TimelineMax();
+
+  tl3.fromTo(slider, 1.5, {x: "-100%",y:"100%"},{y: "0%",x:"0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(slide1, 1.5, {opacity: 0,x:"30%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(slide2, 1.5, {opacity: 0,x:"30%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(slide3, 1.5, {opacity: 0,x:"30%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(slide4, 1.5, {opacity: 0,x:"30%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(slide5, 1.5, {opacity: 0,x:"30%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(slide6, 1.5, {opacity: 0,x:"30%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=1.2")
+  .fromTo(headline,1.5,{opacity: 0,x:"30%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=2.2")
+  .fromTo(heroimg,1.5,{opacity: 0,x:"10%"},{opacity: 1,x:"0%", ease: Power2.easeInOut },"-=1.5");
   
   
+ 
+  // var tl4 = new TimelineMax();
+  // $(heroimg).mouseenter(function(){
+  //   tl4.to(this, 1, {x:"-30%",height:"50%"})
+  // })
   
-  document.addEventListener('mousemove',e => {
-    cursor.setAttribute("style", "top:" + (e.pageY - 10 ) + "px; left:" + ( e.pageX - 10 ) + "px;");
-  })
+  // $(heroimg).mouseleave(function(){
+  //   tl4.to(this, 1, {height:"40%",x:"-20%"})
+  // })
+
+  // portfolio animation
 
 
 });
